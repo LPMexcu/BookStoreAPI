6efe0e8b248243ebfef955df8301e675875a6917
Marcos Lopez <71472051+LPMexcu@users.noreply.github.com>
2024-03-23 09:52:23 -0600

    Agregar archivos de proyecto.

---
+++ BookStoreAPI/Controllers/BooksController.cs
@@ -0,0 +1,67 @@
+﻿using BookStoreAPI.Models;
+using BookStoreAPI.Services;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookStoreAPI.Controllers 
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BooksController : ControllerBase
+    {
+        private readonly BooksService _booksService;
+        public BooksController(BooksService booksService) => _booksService = booksService;
+
+        [HttpGet]
+        public async Task<List<Book>> Get() =>
+            await _booksService.GetAsync();
+
+        [HttpPost]
+        public async Task<IActionResult> Post(Book newBook)
+        {
+            await _booksService.CreateAsync(newBook);
+
+            return CreatedAtAction(nameof(Get), new {id=newBook.Id}, newBook);
+        }
+
+        [HttpGet("{id:length(24)}")]
+        public async Task<ActionResult<Book>> Get(string id)
+        {
+            var book = await _booksService.GetAsync(id);
+
+            if (book is null)
+            {
+                return NotFound();
+            }
+            return book;
+        }
+
+
+
+        [HttpPut("{id:length(24)}")]
+        public async Task<IActionResult> Update(string id, Book updateBook)
+        {
+            var book = await _booksService.GetAsync(id);
+            if (book is null)
+            {
+                return NotFound();
+            }
+
+            updateBook.Id = book.Id;
+
+            await _booksService.UpdateAsync(id, updateBook);
+            return NoContent();
+        }
+
+        [HttpDelete("{id:length(24)}")]
+        public async Task<IActionResult> Delete(string id)
+        {
+            var book = await _booksService.GetAsync(id);
+
+            if (book is null)
+            { return NotFound(); }
+            await _booksService.RemoveAsync(id);
+            return NoContent();
+        }
+    }
+}
